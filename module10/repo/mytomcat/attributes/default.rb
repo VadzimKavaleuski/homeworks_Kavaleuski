704554f3322f400fd11a67b0c7b0813c456a47dd
VadimKA <vadimka@rd-epam.com>
2019-04-08 17:53:44 +0000

    commit change to tomcat recipes

--- module10/repo/mytomcat/attributes/default.rb
+++ module10/repo/mytomcat/attributes/default.rb
@@ -1,3 +1,3 @@
-# create by jenkins job 08/04/2019 17:44:52
+# create by jenkins job 08/04/2019 17:52:54
 node.default['tomcatapp']['image']['name']='tomcat_app'
 node.default['tomcatapp']['image']['tag']='1'
